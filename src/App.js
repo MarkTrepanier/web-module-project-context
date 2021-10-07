16e3cd5b46bdaf12c88dffac6a96661c6328ff72
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-07 15:43:50 -0400

    products contet created. Products sorted

--- src/App.js
+++ src/App.js
@@ -6,28 +6,31 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import {productContext} from './context/productcontext'
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart,item])
 	};
 
 	return (
+		<productContext.Provider value={{products,addItem}}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products/>
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
 		</div>
+		</productContext.Provider>
 	);
 }
 
