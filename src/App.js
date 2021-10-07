30c3c8f65acf602d66281a3fdbb871d4e192e1b5
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-07 16:02:30 -0400

    cart refactored. nav and shopping props ref removed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-import {productContext} from './context/productcontext'
-
+import {productContext} from './context/productcontext';
+import {cartContext} from './context/cartContext';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -18,8 +18,9 @@ function App() {
 
 	return (
 		<productContext.Provider value={{products,addItem}}>
+		<cartContext.Provider value={{cart}}>
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation />
 
 			{/* Routes */}
 			<Route exact path="/">
@@ -27,9 +28,10 @@ function App() {
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart/>
 			</Route>
 		</div>
+		</cartContext.Provider>
 		</productContext.Provider>
 	);
 }
